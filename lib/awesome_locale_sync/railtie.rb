aab23f33e9fb6f4638da5537d374191b7acaca91
Gintaras Sakalauskas <gintaras.sakalauskas@gmail.com>
2012-04-15 15:37:12 +0300

    Removed deprecated stuff

--- lib/awesome_locale_sync/railtie.rb
+++ lib/awesome_locale_sync/railtie.rb
@@ -2,8 +2,6 @@ require 'awesome_locale_sync'
 require 'rails'
 module AwesomeLocaleSync
   class Railtie < Rails::Railtie
-    railtie_name :awesome_locale_sync
-
     rake_tasks do
       load "tasks/sync.rake"
     end
